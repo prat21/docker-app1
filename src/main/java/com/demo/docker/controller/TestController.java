014bf46ef5028daa93dbf373117c455c7fd7cf05
prat21 <prmukherjee.21@gmail.com>
2025-04-16 01:58:59 +0530

    Initial Commit

--- src/main/java/com/demo/docker/controller/TestController.java
+++ src/main/java/com/demo/docker/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.demo.docker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/docker/app1")
+@RestController
+@RequestMapping("/docker/app1")
 public class TestController {
 
     @GetMapping("/test")
